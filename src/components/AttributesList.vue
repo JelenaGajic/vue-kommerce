fb1d1d928b867ea4857e82629d35c76f12b0e677
Jelena Gajic <jelena.gajic@vast.com>
2023-01-17 15:59:30 +0100

    Set up vue proxy

---
+++ src/components/AttributesList.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Attributes</h1>
+    </div>
+</template>
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,14 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8081'
+        // pathRewrite: { "^/api/": "/" },
+        // changeOrigin: true,
+        // logLevel: 'debug'
+      }
+    }
+  }
 });
